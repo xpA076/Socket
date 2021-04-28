2a7be9094c810d380ae913d8e82f969d6495bd60
xpA076 <781398368@qq.com>
2021-04-28 12:04:42 +0800

    Create ConfirmWindowViewModel.cs

---
+++ FileManager/ViewModels/ConfirmWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.ViewModels
+{
+    public enum ConfirmWindowType : int
+    {
+        Download,
+        Upload
+    }
+
+    public class ConfirmWindowViewModel
+    {
+    }
+}
