2fb38ce8d11aa1fa4d41f078e8f832ef9fc643f1
xpA076 <781398368@qq.com>
2021-05-27 11:56:49 +0800

    sth

--- FileManager/SocketLib/Models/SocketServerFileStreamInfo.cs
+++ FileManager/SocketLib/Models/SocketServerFileStreamInfo.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileManager.SocketLib.SocketServer
+namespace FileManager.SocketLib
 {
     public class SocketServerFileStreamInfo
     {
